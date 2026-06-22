1606eaf3c3676587d56fa895d4885df2137256b4
harshasagark-pixel <harshasagark@gmail.com>
2026-06-22 16:21:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,71 @@
-let currentSlide = 0;
+function showMessage(choice){
 
-const slides = document.querySelectorAll(".slide");
+document.getElementById("buttons").style.display = "none";
 
-function nextSlide() {
+let text = "";
 
-    slides[currentSlide].classList.remove("active");
+if(choice==1){
 
-    currentSlide++;
+text = `
+I know I made mistakes, and I understand that my actions hurt you.
 
-    if(currentSlide >= slides.length){
-        currentSlide = slides.length - 1;
-    }
+I am not trying to justify myself or ask for immediate forgiveness.
 
-    slides[currentSlide].classList.add("active");
+I only wanted to acknowledge where I was wrong and sincerely apologize.
+
+I respect your feelings and understand if you need time.
+`;
+
+}
+
+if(choice==2){
+
+text = `
+This experience taught me many things.
+
+It made me understand the importance of trust, words, and actions.
+
+I realized that friendships should never be taken for granted.
+
+I have learned from my mistakes and hope to become a better person because of them.
+`;
+
+}
+
+if(choice==3){
+
+text = `
+Despite everything that happened, I want to thank you wholeheartedly.
+
+Thank you for the conversations, the memories, the laughter, and the moments that made our friendship special.
+
+I remain grateful for having known you and for the friendship we shared.
+
+Some memories are valuable not because they last forever, but because they once brought happiness.
+
+And for that, I will always be thankful.
+`;
+
+}
+
+if(choice==4){
+
+text = `
+I don't want this to be a goodbye.
+
+I understand that I made mistakes and that some wounds need time to heal.
+
+I am not expecting things to become normal immediately.
+
+All I hope is that, whenever you feel ready, there might still be a place for our friendship again.
+
+Until then, I will respect your space and wish you nothing but peace, happiness, and success.
+
+Take care, Keerthana.
+`;
+
+}
+
+document.getElementById("message").innerText = text;
 
 }
