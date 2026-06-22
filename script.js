86a332203e47f508c5a96022271cf02c98f8f1be
harshasagark-pixel <harshasagark@gmail.com>
2026-06-22 16:05:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+let currentSlide = 0;
+
+const slides = document.querySelectorAll(".slide");
+
 function nextSlide() {
-    alert("More slides coming...");
+
+    slides[currentSlide].classList.remove("active");
+
+    currentSlide++;
+
+    if(currentSlide >= slides.length){
+        currentSlide = slides.length - 1;
+    }
+
+    slides[currentSlide].classList.add("active");
 }
